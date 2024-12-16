3514193e4c00f6a3bf2dc43da940684ca31d19db
Mossad-26 <mossadmohamed6070@gmail.com>
2024-12-17 01:38:41 +0200

    .js

---
+++ Product.js
@@ -0,0 +1,15 @@
+// scrolling up
+let scrolling = document.querySelector(".arrow");
+window.onscroll = function () {
+if (this.scrollY >= 1000) {
+    scrolling.classList.add("show");
+} else {
+    scrolling.classList.remove("show");
+}
+};
+scrolling.onclick = function () {
+window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+});
+};
